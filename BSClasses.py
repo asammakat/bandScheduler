e3236f6547677d21b3f52b91cdc65a498cd96228
Adam Sammakia <adam.sammakia@gmail.com>
2019-12-16 17:35:13 -0600

    Trying to save MessingWithFunctions

--- BSClasses.py
+++ BSClasses.py
@@ -1,7 +1,6 @@
 import datetime
 import csv
 
-##THIS IS A TEST
 #################################################################################
 
 class Band:
@@ -20,6 +19,94 @@ class Band:
         self.roster.append(name)
     def add_availability(self, date):
         self.availability.append(date)
+
+    def remove_old_dates(self):
+        #csv: name, year, month, day, startHour, startMinute, endHour, endMinute
+        #for datetime: year, month, day[,hour,minute,second,mocrosecond,tzinfo]
+        for member in self.roster:
+            for date in enumerate(member.get_availability()):
+                member_date = date[1]
+                endtime = datetime.datetime(int(member_date[1]),
+                                            int(member_date[2]),
+                                            int(member_date[3]),
+                                            int(member_date[6]),
+                                            int(member_date[7]))
+                if endtime < datetime.datetime.now():
+                    #delete_availability uses index so use date[0]
+                    member.delete_availability(date[0])
+
+    def find_dates_in_common(self):
+        #csv: name, year, month, day, startHour, startMinute, endHour, endMinute
+        #for datetime: year, month, day[,hour,minute,second,mocrosecond,tzinfo]
+        dates_in_common = [];
+
+        #go through dates for first member in band.roster
+        for date in self.roster[0].get_availability():
+            #flag used to determine if a start/endtime sholud be appended to dates_in_common
+            green_light = True 
+
+            #start time for first member as dt obj, gets adjusted
+            #according to availability of other members
+            start_time = datetime.datetime( 
+                int(date[1]),int(date[2]),
+                int(date[3]),int(date[4]),
+                int(date[5])
+                )
+            
+            #end for first member as dt obj, gets adjusted
+            #according to availability of other members
+            end_time = datetime.datetime(
+                int(date[1]),int(date[2]),
+                int(date[3]),int(date[6]),
+                int(date[7])
+                )
+                     
+            for member in self.roster[1:]: #go through the rest of band.roster
+                #flag to determine if a change has been made to start_time or end_time
+                change_made = False
+                #go through member's availability
+                for member_date in member.get_availability():
+
+                    #start time for member as dt obj
+                    member_start_time = datetime.datetime(
+                        int(member_date[1]),int(member_date[2]),
+                        int(member_date[3]),int(member_date[4]),
+                        int(member_date[5])
+                        )
+
+                    #end time for member as dt obj
+                    member_end_time = datetime.datetime(
+                        int(member_date[1]),int(member_date[2]),
+                        int(member_date[3]),int(member_date[6]),
+                        int(member_date[7])
+                        )
+
+                    # If member start time is in the desired window
+                    #change start time outside of this loop
+                    if (member_start_time >= start_time) and (member_start_time < end_time):
+                        start_time = member_start_time
+                        change_made = True 
+
+                    #If member end time is in the desired window
+                    #change end time outside of this loop
+                    if(member_end_time > start_time) and (member_end_time <= end_time):
+                        end_time = member_end_time                           
+                        change_made = True
+
+                #if no change made we know the date in incompatable
+                if change_made == False:  
+                    green_light = False
+                    break #Current date is incompatable so move on to the next
+
+            if green_light == True: #current date is compatable
+                dates_in_common.append(["Everybody", str(start_time.year),
+                                        str(start_time.month), str(start_time.day),
+                                        str(start_time.hour), str(start_time.minute),
+                                        str(end_time.hour), str(end_time.minute)])
+
+        return dates_in_common #[[st1,et1],[st2,et2], ... ]
+            
+                
 #################################################################################
 
 class Player:
@@ -50,6 +137,22 @@ class Player:
         for i in range(len(self.availability)):
             print(i+1, ": ", format_date_text(self.availability[i]), sep = "")
 #################################################################################
+
+def remove_old_dates(band):
+    #in csv: name, year, month, day, startHour, startMinute, endHour, endMinute
+    #for datetime: year, month, day[,hour,minute,second,mocrosecond,tzinfo]
+    for member in band.get_roster():
+        for date in enumerate(member.get_availability()):
+            member_date = date[1]
+            endtime = datetime.datetime(int(member_date[1]),
+                                        int(member_date[2]),
+                                        int(member_date[3]),
+                                        int(member_date[6]),
+                                        int(member_date[7]))
+            if endtime < datetime.datetime.now():
+                #delete_availability uses index so use date[0]
+                member.delete_availability(date[0])
+#################################################################################    
             
 def update_csv(band_member_list):
     
@@ -84,11 +187,13 @@ def load_availability(band_member_list):
                 availability.append(row)
             line_count = line_count + 1
 
-        #deal with 0, convert to 00
-        for date in range(len(availability)):
-            for item in range(len(availability[date])):
-                if availability[date][item] == '0':
-                    availability[date][item] = "00"
+    availability = deal_with_zeros(availability)
+
+##        #deal with 0, convert to 00
+##        for date in range(len(availability)):
+##            for item in range(len(availability[date])):
+##                if availability[date][item] == '0':
+##                    availability[date][item] = "00"
 
     #add availability to player objects
     for player in band_member_list:
@@ -114,7 +219,6 @@ def format_date_text(date):
 #--------------------------------------------------------------------------------
 
 def display_availability(dates):
-    print("Your availability is: \n" )
     for date in dates:
         print("\t", format_date_text(date))
     print("\n")
@@ -125,6 +229,17 @@ def display_band_availability(band):
         print(member.get_name(), "-"*40)
         for date in member.get_availability():
             print("\t", format_date_text(date))
+
+def deal_with_zeros(dates):
+    #deal with 0, convert to 00
+    for date in range(len(dates)):
+        for item in range(len(dates[date])):
+            if dates[date][item] == '0':
+                print("HIT!")
+                dates[date][item] = "00"
+    print(dates)
+    return dates
+
             
 #################################################################################
 
@@ -173,7 +288,7 @@ def add_availability(band_member):
         new_entry.append(end_time[1])
         
         #TODO Error check user input
-        #TODO Impliment user inputing
+        #TODO Impliment user inputing a or p for am / pm
 
         print("You are available on %s/%s/%s from %s:%s to %s:%s\n"
                                         %(new_entry[2], new_entry[3],
@@ -191,6 +306,7 @@ def add_availability(band_member):
         add_another = input()
         if add_another != "y":
             break
+#################################################################################
 
         
 def main():
@@ -217,15 +333,29 @@ def main():
     
     band_member = band_member_list[int(input()) -1]
     
-    ####TODO use date time objects to remove dates from the list that 
-    #have already passed####
     #loads each band member with their current availability
-    load_availability(band_member_list) 
+    load_availability(band_member_list)
+
+    #remove davailability dates that have passed
+    band.remove_old_dates()
+
+    #load group availability
+    group_availability = [band.find_dates_in_common()]
+    group_availability = deal_with_zeros(group_availability)
     
     #display availability
     print("\n")
+    print("Your availability is: \n" )
     display_band_availability(band)
     print("\n")#newline after displaying band_availability
+
+    #display group availability
+    if len(group_availability) == 0:
+        print("There are currently no dates where everyone is available")
+    else:
+        print("Current group availability is: ")
+        for date in group_availability:
+            display_availability(date)
 
     user_input = 0
     while(True):
@@ -235,6 +365,7 @@ def main():
                "    Press 2 to delete availability\n"
                "    Press 3 to see all availability\n"
                "    Press 4 to see your availability\n"
+               "    Press 5 to see availability in common\n"              
                "    Enter -1 to exit")
         user_input = int(input())
 
@@ -247,6 +378,9 @@ def main():
             display_band_availability(band)
         elif user_input == 4:            
             display_availability(band_member.get_availability())
+        elif user_input == 5:
+            print("Current group availability is: ")
+            display_availability(band.find_dates_in_common())
         elif user_input == -1:
             update_csv(band.get_roster())
             print("goodbye")
